f60dbf3e6184e58f8e06854ed2185a2ba96892ac
59de44955ebd <59de44955ebd@gmail.com>
2026-08-07 13:02:34 +0200

    updated winapp.py

--- src/webview2/embed/winapp.py
+++ src/webview2/embed/winapp.py
@@ -1,3 +1,12 @@
+"""
+WebView2 can be loaded directly into any WinApp window (e.g. a MainWin)
+by specifying its HWND, and then be resized whenever the container windows
+was resized.
+
+Therefor this wrapper here is purely optional, it creates an additional
+window into which a webview is loaded, and then automatically resizes
+the webview when this window is resized.
+"""
 from .. import *
 from ..winapp.const import *
 from ..winapp.themes import *
@@ -12,8 +21,13 @@ class WebView2(WebView2):
     ########################################
     #
     ########################################
-    def __init__(self, parent_window, *args, **kwargs):
-
+    def __init__(
+        self,
+        parent_window,
+        *args,
+        left = 0, top = 0, width = 0, height = 0,
+        **kwargs
+    ):
         self._controller = None
 
         ########################################
@@ -38,7 +52,7 @@ class WebView2(WebView2):
         self.window = Window(
             newclass.lpszClassName,
             parent_window = parent_window,
-#            window_title = window_title,
+            left = left, top = top, width = width, height = height
         )
 
         super().__init__(parent_hwnd = self.window.hwnd, *args, **kwargs)
